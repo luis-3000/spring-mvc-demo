4608c579b109ebdd2055aa6fc1fdec1f6fd5a9c2
Jose Luis Castillo <josecastillocs@gmail.com>
2020-06-10 21:49:35 -0500

    Implemented validation using a regular expression (regex) with Spring MVC Form Validation.  1). Added validation rule to the Customer class. 2). Displayed error messages on the customer-form HTML form. 3). Performed validation in the CustomerController class.  4). Updated the confirmation page. Tested successfully.

--- src/com/joselcastillo/springdemo/mvc/Customer.java
+++ src/com/joselcastillo/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.joselcastillo.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -14,9 +15,14 @@ public class Customer {
 	@Size(min=1, message="last name is required")
 	private String lastName;
 	
+	// Adding validation rules
 	@Min(value=0, message="value must be greater than or equal to zero")
 	@Max(value=10, message="value must be less than or equal to 10")
 	private int freePasses;
+	
+	// Adding validation rules
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 	
 	public String getFirstName() {
 		return firstName;
@@ -40,6 +46,14 @@ public class Customer {
 
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 	
