1a31973a73ec8cf5d7e4ec5c9e1509f3f1f98db0
Jose Luis Castillo <josecastillocs@gmail.com>
2020-06-10 20:36:49 -0500

    Implemented validation of a number range: min, max with Spring MVC Form Validation. 1). Added validation rule to the Customer class. 2). Displayed error messages on the customer-form HTML form. 3). Performed validation in the CustomerController class.  4). Updated the confirmation page. Tested successfully.

--- src/com/joselcastillo/springdemo/mvc/Customer.java
+++ src/com/joselcastillo/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.joselcastillo.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,6 +13,10 @@ public class Customer {
 	@NotNull(message="last name is required")
 	@Size(min=1, message="last name is required")
 	private String lastName;
+	
+	@Min(value=0, message="value must be greater than or equal to zero")
+	@Max(value=10, message="value must be less than or equal to 10")
+	private int freePasses;
 	
 	public String getFirstName() {
 		return firstName;
@@ -26,6 +32,14 @@ public class Customer {
 	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
 	}
 	
 	
